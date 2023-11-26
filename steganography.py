bce0acf992a108810860a4d3821d8c2fb04b9fc5
albertex <albertexye@gmail.com>
2023-11-25 21:46:18 -0500

    The change corresponding to the change in C extension

--- steganography.py
+++ steganography.py
@@ -1,128 +1,314 @@
 import ctypes
-import math
-from typing import BinaryIO
+from enum import Enum
+from typing import BinaryIO, Sequence
 
 from PIL import Image
 
-SQUARE_SIZE = 8
+# C data structure definitions
+SQUARE_SIZE = 16
 
+
+class CSquare(ctypes.Structure):
+    """An internal C struct representing the data structure of a square"""
+
+    _fields_ = (
+        ('x', ctypes.c_uint64),
+        ('y', ctypes.c_uint64),
+        ('entropy', ctypes.c_double),
+    )
+
+
+class CSquareList(ctypes.Structure):
+    """An internal C struct representing the data structure of **list[CSquare]**"""
+
+    _fields_ = (
+        ('squares', ctypes.POINTER(CSquare)),
+        ('len', ctypes.c_uint64),
+    )
+
+
+class CImage(ctypes.Structure):
+    """A C struct representing the data structure of **PIL.Image.Image**"""
+
+    _fields_ = (
+        ('w', ctypes.c_uint64),
+        ('h', ctypes.c_uint64),
+        ('c', ctypes.c_uint64),
+        ('pixels', ctypes.POINTER(ctypes.c_uint8)),
+        ('squareList', CSquareList),
+        ('usage', ctypes.c_uint64),
+        ('copied', ctypes.c_bool),
+    )
+
+    def __bytes__(self) -> bytes:
+        return self.get_pixels()
+
+    def get_pixels(self) -> bytes:
+        return bytes(ctypes.cast(self.pixels, ctypes.POINTER(ctypes.c_uint8 * self.w * self.h * self.c)).contents)
+
+    @classmethod
+    def new_image(cls, image: Image.Image):
+        channels = len(image.getbands())
+        pixels = image.tobytes()
+        buffer = (ctypes.c_uint8 * len(pixels)).from_buffer(bytearray(pixels))
+        return cls(image.width, image.height, channels, buffer, CSquareList(None, 0), 0, False)
+
+
+class CImageList(ctypes.Structure):
+    """A C struct representing the data structure of **list[PIL.Image.Image]**"""
+
+    _fields_ = (
+        ('images', ctypes.POINTER(CImage)),
+        ('len', ctypes.c_uint64),
+    )
+
+    def get_images(self) -> list[CImage]:
+        return list(ctypes.cast(self.images, ctypes.POINTER(CImage * self.len)).contents)
+
+
+class CData(ctypes.Structure):
+    """A C struct representing the data structure of **bytes**"""
+
+    _fields_ = (
+        ('data', ctypes.POINTER(ctypes.c_uint8)),
+        ('len', ctypes.c_uint64),
+        ('padded', ctypes.c_bool),
+    )
+
+    def __bytes__(self):
+        return self.get_data()
+
+    def get_data(self) -> bytes:
+        return bytes(ctypes.cast(self.data, ctypes.POINTER(ctypes.c_uint8 * self.len)).contents)
+
+
+class CDataPieces(ctypes.Structure):
+    """A C struct representing the data structure of **list[bytes]**"""
+
+    _fields_ = (
+        ('pieces', ctypes.POINTER(CData)),
+        ('len', ctypes.c_uint64),
+    )
+
+    def get_pieces(self) -> list[bytes]:
+        return [bytes(i) for i in list(ctypes.cast(self.pieces, ctypes.POINTER(CData * self.len)).contents)]
+
+
+class CPrecomputed(ctypes.Structure):
+    """A C struct representing the result of function **precompute**"""
+
+    _fields_ = (
+        ('imageList', CImageList),
+        ('code', ctypes.c_uint64),
+    )
+
+    def get_lengths(self, reserved: int) -> tuple[int]:
+        """Gets the lengths of data pieces (without structure size)
+
+        :param reserved: the reserved size for structures
+        :return: the lengths of pieces without structure size
+        """
+
+        result: list[int] = []
+        for c_image in self.imageList.get_images():
+            result.append(c_image.usage * (SQUARE_SIZE * SQUARE_SIZE * c_image.c) - reserved)
+        return tuple(result)
+
+
+class CExtracted(ctypes.Structure):
+    """A C struct representing the result of function **extract**"""
+
+    _fields_ = (
+        ('data', CData),
+        ('code', ctypes.c_uint64),
+    )
+
+
+# typedef CPrecomputed CEmbedded;
+CEmbedded = CPrecomputed
+
+# C function definitions
 DLL = ctypes.CDLL("./libstegano.dll")
 
+# Precomputed precompute(ImageList imageList, uint64_t dataLen, uint64_t reserved);
+precompute: ctypes.CFUNCTYPE = DLL.precompute
+precompute.argtypes = (CImageList, ctypes.c_uint64, ctypes.c_uint64)
+precompute.restype = CPrecomputed
+
+# Embedded embed(Precomputed precomputed, DataPieces dataPieces);
 embed: ctypes.CFUNCTYPE = DLL.embed
-embed.argtypes = (ctypes.c_char_p, ctypes.c_uint64, ctypes.c_char_p, ctypes.c_uint64, ctypes.c_uint64, ctypes.c_uint64)
-embed.restype = ctypes.c_void_p
+embed.argtypes = (CPrecomputed, CDataPieces)
+embed.restype = CEmbedded
 
-extract = DLL.extract
-extract.argtypes = (ctypes.c_char_p, ctypes.c_uint64, ctypes.c_uint64, ctypes.c_uint64)
-extract.restype = ctypes.c_void_p
+# Extracted extract(Image image, uint64_t reserved);
+extract: ctypes.CFUNCTYPE = DLL.extract
+extract.argtypes = (CImage, ctypes.c_uint64)
+extract.restype = CExtracted
 
-get_data = DLL.get_data
-get_data.argtypes = (ctypes.c_void_p,)
-get_data.restype = ctypes.POINTER(ctypes.c_char)
+# void free_precomputed(Precomputed precomputed);
+free_precomputed: ctypes.CFUNCTYPE = DLL.free_precomputed
+free_precomputed.argtypes = (CPrecomputed,)
+free_precomputed.restype = None
 
-get_len = DLL.get_len
-get_len.argtypes = (ctypes.c_void_p,)
-get_len.restype = ctypes.c_uint64
+# void free_extracted(Extracted extracted);
+free_extracted: ctypes.CFUNCTYPE = DLL.free_extracted
+free_extracted.argtypes = (CExtracted,)
+free_extracted.restype = None
 
-free = DLL.free_
-free.argtypes = (ctypes.c_void_p,)
+
+class CStatus(Enum):
+    """The error code of the C functions"""
+
+    OK = 0
+    AllocationFailure = 1  # alloc(...) == NULL
+    OversizedData = 2  # the images can't contain the data
+    BadDataPiecesLen = 3  # DataPieces.len != Precomputed.imageList.len
+    BadPrecomputed = 4  # a Precomputed with an error code is passed to embed
+    InvalidLen = 5  # invalid length of data in the extracted image
 
 
 class Steganography:
-    images: list[tuple[BinaryIO, BinaryIO]]
-    precomputed: list[tuple[int, Image.Image]]
-    is_precomputed: bool
-    structure_length: int
+    """
+    A class for embedding and extracting data from images.
+    For embedding, an object has to be constructed.
+    For extracting, no object needs to be constructed, instead only a class method is used.
+    """
 
-    def __init__(self, structure_length: int):
+    images: list[tuple[BinaryIO, BinaryIO]]  # a list containing the images added
+    modes: list[str]  # a list that stores the mode of each image
+    precomputed: CPrecomputed
+    is_precomputed: bool
+    reserved: int  # the reserved size for data structure
+    data_len: int
+
+    def __init__(self, reserved: int):
+        """
+        :param reserved: the reserved size for data structure
+        """
+
         self.images = []
-        self.precomputed = []
+        self.modes = []
         self.is_precomputed = False
-        self.structure_length = structure_length
+        self.reserved = reserved
+
+    @staticmethod
+    def __handle_error_code(code: int):
+        match code:
+            case CStatus.AllocationFailure:
+                raise MemoryError("memory allocation failure in CDLL")
+            case CStatus.OversizedData.value:
+                raise ValueError("oversized data")
+            case CStatus.BadDataPiecesLen.value:
+                raise ValueError("precomputed data is invalid: invalid len")
+            case CStatus.BadPrecomputed:
+                raise ValueError("precomputed data have an error code")
+            case CStatus.InvalidLen.value:
+                raise ValueError("invalid image: invalid data length")
 
     def add_image(self, file_src: BinaryIO, file_dst: BinaryIO) -> None:
+        """Adds an image to the image list. For embedding only.
+
+        :param file_src: the source file that is readable
+        :param file_dst: the destiny file that is writable
+        *file_src* may be the same object as *file_dst*.
+        *file_src* and *file_dst* will not be closed until **clear** is called.
+        """
+
         self.images.append((file_src, file_dst))
+        if self.is_precomputed:
+            free_precomputed(self.precomputed)
         self.is_precomputed = False
 
     def clear(self) -> None:
+        """Clears the image list. For embedding only."""
+
         for src, dst in self.images:
             if not src.closed:
                 src.close()
             if not dst.closed:
                 dst.close()
         self.images.clear()
+        self.modes.clear()
         if self.is_precomputed:
-            for _, image in self.precomputed:
-                image.close()
-            self.is_precomputed = False
+            free_precomputed(self.precomputed)
+        self.is_precomputed = False
 
     def precompute(self, data_length: int) -> None:
+        """Precomputes the images. This method has to be called before embedding.
+
+        :param data_length: The length of the data being embedded (without structure size)
+        """
+
         if self.is_precomputed:
             return
+
+        self.modes.clear()
+        self.data_len = data_length
+
+        image_list = CImageList((CImage * len(self.images))(), len(self.images))
+
+        for i, (reader, _) in enumerate(self.images):
+            image = Image.open(reader)
+            self.modes.append(image.mode)
+
+            c_image = CImage.new_image(image)
+            image_list.images[i] = c_image
+            image.close()
+
+        self.precomputed = precompute(image_list, data_length, self.reserved)
+
+        self.__handle_error_code(self.precomputed.code)
         self.is_precomputed = True
 
-        precomputed = []
-        entropy_sum = 0.
-        for reader, _ in self.images:
-            image = Image.open(reader)
-            channels = len(image.getbands())
-            length = int((int(image.width / SQUARE_SIZE) * int(
-                image.height / SQUARE_SIZE) - 1) * SQUARE_SIZE * SQUARE_SIZE * channels / 8)
-            entropy = image.entropy()
-            entropy_sum += entropy
-            precomputed.append((entropy, length, image))
+    def embed(self, pieces: Sequence[bytes], format_: str = "PNG"):
+        """Embeds pieces of data into the images. Call this method after calling **precompute**.
 
-        passed = False
-        while not passed:
-            passed = True
-            for entropy, length, image in precomputed:
-                if length < math.ceil(data_length * (entropy / entropy_sum)) + self.structure_length:
-                    passed = False
-                    entropy_sum -= entropy
-                    image.close()
-                    precomputed.remove((entropy, length, image))
-                    break
+        :param pieces: the pieces of data being embedded, including the structures
+        :param format_: the format for the embedded images
+        """
 
-        for entropy, length, image in precomputed:
-            self.precomputed.append((math.ceil(data_length * (entropy / entropy_sum)), image))
-
-    def embed(self, pieces: list[bytes], format_: str = "PNG"):
         if not self.precomputed:
-            raise RuntimeError("Precomputation has not been done. Call the precompute method first.")
-        for i in range(len(pieces)):
-            if len(pieces[i]) != self.precomputed[i][0] + self.structure_length:
-                raise ValueError(f"The length of pieces[{i}] is not correct. ")
+            raise RuntimeError("precomputation has not been done. Call the precompute method first.")
 
+        # build CDataPieces struct
+        data_pieces = CDataPieces((CData * len(pieces))(), len(pieces))
         for i in range(len(pieces)):
-            image = self.precomputed[i][1]
-            channels = len(image.getbands())
-            raw = embed(
-                pieces[i],
-                ctypes.c_uint64(len(pieces[i])),
-                image.tobytes(),
-                ctypes.c_uint64(image.width),
-                ctypes.c_uint64(image.height),
-                ctypes.c_uint64(channels)
-            )
+            piece = pieces[i]
+            buffer = (ctypes.c_uint8 * len(piece)).from_buffer(bytearray(piece))
+            data_pieces.pieces[i] = CData(buffer, len(piece))
+
+        # call embedded
+        embedded: CEmbedded = embed(self.precomputed, data_pieces)
+
+        # handle errors
+        self.__handle_error_code(embedded.code)
+
+        # save results
+        for i, c_image in enumerate(embedded.imageList.get_images()):
+            c_image: CImage
+            image = Image.frombuffer(self.modes[i], (c_image.w, c_image.h), c_image.get_pixels())
+            writer = self.images[i][1]
+            writer.truncate(0)
+            image.save(writer, format_)
             image.close()
-            embedded = bytes(
-                ctypes.cast(raw, ctypes.POINTER(ctypes.c_char * (image.width * image.height * channels))).contents)
-            free(raw)
 
-            new_image = Image.frombuffer(image.mode, image.size, embedded)
-            self.images[i][1].truncate(0)
-            new_image.save(self.images[i][1], format_)
-            self.images[i][1].close()
-            new_image.close()
+    @classmethod
+    def extract(cls, src: BinaryIO, reserved: int) -> bytes:
+        """Extracts the data from **src**
 
-    @staticmethod
-    def extract(src: BinaryIO) -> bytes:
+        **src** will not be closed, you have to close it somewhere
+        :param src: the source image
+        :param reserved: the reserved size for structure
+        :return: the extracted data
+        """
+
         image = Image.open(src)
-        data: ctypes.c_void_p = extract(image.tobytes(), image.width, image.height, len(image.getbands()))
+        c_image = CImage.new_image(image)
+        extracted: CExtracted = extract(c_image, ctypes.c_uint64(reserved))
         image.close()
-        src.close()
-        result_len = get_len(data)
-        result_ptr = get_data(data)
-        result = bytes(ctypes.cast(result_ptr, ctypes.POINTER(ctypes.c_char * result_len)).contents)
-        free(data)
+
+        cls.__handle_error_code(extracted.code)
+
+        result = bytes(extracted.data)
+        free_extracted(extracted)
         return result
--- steganography_test.py
+++ steganography_test.py
@@ -1,10 +1,56 @@
+import os
 import unittest
+
 import steganography
 
 
 class SteganographyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_normal(self):
+        reserved = 130
+        data_len = 23576
+        msg = os.urandom(data_len + reserved)
+
+        with open("test_image.png", "rb") as src, open("embedded.png", "wb") as dst:
+            obj = steganography.Steganography(reserved)
+            obj.add_image(src, dst)
+            obj.precompute(data_len)
+            obj.embed([msg])
+            obj.clear()
+
+        with open("embedded.png", "rb") as src:
+            extracted = steganography.Steganography.extract(src, reserved)
+
+        self.assertEqual(extracted, msg)
+
+    def test_oversize(self):
+        reserved = 130
+        data_len = 9999999
+        msg = os.urandom(data_len + reserved)
+
+        try:
+            with open("test_image.png", "rb") as src, open("embedded.png", "wb") as dst:
+                obj = steganography.Steganography(reserved)
+                obj.add_image(src, dst)
+                obj.precompute(data_len)
+                obj.embed([msg])
+                obj.clear()
+        except ValueError as e:
+            print(e)
+            return
+
+        self.fail("no error raised on oversized data")
+
+    def test_invalid_image(self):
+        reserved = 130
+
+        try:
+            with open("test_image.png", "rb") as src:
+                steganography.Steganography.extract(src, reserved)
+        except ValueError as e:
+            print(e)
+            return
+
+        self.fail("no error raised on invalid image")
 
 
 if __name__ == '__main__':
